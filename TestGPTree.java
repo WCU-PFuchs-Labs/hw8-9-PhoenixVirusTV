ea5fdff95b1c754744010fff84a74495015d0943
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-02 16:49:41 -0500

    TestGPTree.java

--- TestGPTree.java
+++ TestGPTree.java
@@ -1,23 +1,20 @@
 import java.util.*;
+import java.io.*;
 
-public class TestGPTree {
-    static int numIndepVars = 3;
-    static int maxDepth = 5;
-    static Random rand = new Random();
-
+public class TestGP {
     public static void main(String[] args) {
-        double[] data = {3.14, 2.78, 1.0};
-        Binop[] ops = {new Plus(), new Minus(), new Mult(), new Divide()};
-        NodeFactory n = new NodeFactory(ops, numIndepVars);
 
-        GPTree gpt1 = new GPTree(n, maxDepth, rand);
-        System.out.println(gpt1 + " = " + gpt1.eval(data));
-        GPTree gpt2 = new GPTree(n, maxDepth, rand);
-        System.out.println(gpt2 + " = " + gpt2.eval(data));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter data file name: ");
+        String fileName = sc.nextLine();
 
-        gpt1.crossover(gpt2, rand);
-        System.out.println("After crossover:");
-        System.out.println(gpt1 + " = " + gpt1.eval(data));
-        System.out.println(gpt2 + " = " + gpt2.eval(data));
+        // Create GP manager
+        GP gp = new GP(fileName);
+
+        // Evaluate first generation
+        gp.evalFirstGeneration();
+
+        // Run 50 generations
+        gp.runGenerations(50);
     }
 }
