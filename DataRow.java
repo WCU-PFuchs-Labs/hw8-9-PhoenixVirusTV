4565d7eaf01989835d671ded3df80add6f9beb59
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-02 16:24:31 -0500

    DataRow.java

---
+++ DataRow.java
@@ -0,0 +1,19 @@
+package tabular;
+
+public class DataRow {
+    private double y;
+    private double[] x;
+
+    public DataRow(double y, double[] x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double[] getX() {
+        return x;
+    }
+}
