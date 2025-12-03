8287b98576253fc019bbc967bac90b13abd8fb30
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-03 13:22:17 -0500

    Delete VarNode.java

--- VarNode.java
+++
@@ -1,7 +0,0 @@
-public class VarNode extends Node {
-    private int index;
-    public VarNode(int i){ index=i; }
-    public double eval(double[] x){ return x[index]; }
-    public Node cloneNode(){ return new VarNode(index); }
-    public String toString(){ return "X"+index; }
-}
