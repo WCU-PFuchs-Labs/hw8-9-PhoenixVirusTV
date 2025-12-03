520ed7c5928ce22f13f97825e708b934867102f4
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-03 15:25:02 -0500

    Delete ConstNode.java

--- ConstNode.java
+++
@@ -1,7 +0,0 @@
-public class ConstNode extends Node {
-    private double value;
-    public ConstNode(double v){ value=v; }
-    public double eval(double[] x){ return value; }
-    public Node cloneNode(){ return new ConstNode(value); }
-    public String toString(){ return String.valueOf(value); }
-}
