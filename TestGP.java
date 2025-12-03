6ca2c284c81608d92746dbd1586c3629d2f6e1ec
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-03 17:23:32 -0500

    TestGP.java

---
+++ TestGP.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class TestGP {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter data file name: ");
+        String fileName = in.nextLine();
+
+        Generation gen = new Generation(500, 6, fileName);
+
+        for (int genNum = 1; genNum <= 50; genNum++) {
+            System.out.println("\nGeneration " + genNum + ":");
+
+            gen.evalAll();
+            gen.printBestTree();
+            gen.printBestFitness();
+
+            gen.evolve();
+        }
+    }
+}
