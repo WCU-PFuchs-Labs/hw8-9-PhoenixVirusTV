f860e0f77825bdb609193984bcfddb4c0680457c
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-03 17:19:34 -0500

    DataRow.java

---
+++ DataRow.java
@@ -0,0 +1,12 @@
+public class DataRow {
+    private double y;
+    private double[] xValues;
+
+    public DataRow(double y, double[] xValues) {
+        this.y = y;
+        this.xValues = xValues;
+    }
+
+    public double getY() { return y; }
+    public double[] getXValues() { return xValues; }
+}
