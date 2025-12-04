9f22d12e6c355fba26b1fc647909c144232ad553
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-04 12:44:02 -0500

    Generation.java

--- Generation.java
+++ Generation.java
@@ -8,46 +8,37 @@ public class Generation {
 
     public Generation(int size, int maxDepth, String fileName) {
         data = new DataSet(fileName);
-
-        // Factory uses correct number of independent variables
         Binop[] ops = { new Plus(), new Minus(), new Mult(), new Divide() };
         factory = new NodeFactory(ops, data.getNumIndepVars());
 
         rand = new Random();
         trees = new GPTree[size];
-
         for (int i = 0; i < size; i++) {
             trees[i] = new GPTree(factory, maxDepth, rand);
         }
     }
 
     public void evalAll() {
-        for (GPTree t : trees) {
-            t.evalFitness(data);
-        }
+        for (GPTree t : trees) t.evalFitness(data);
         Arrays.sort(trees);
     }
 
     public ArrayList<GPTree> getTopTen() {
         ArrayList<GPTree> top = new ArrayList<>();
-        for (int i = 0; i < 10 && i < trees.length; i++) {
-            top.add(trees[i]);
-        }
+        for (int i = 0; i < 10 && i < trees.length; i++) top.add(trees[i]);
         return top;
     }
 
     public void printBestFitness() {
-        System.out.println("Best Fitness: " + trees[0].getFitness());
+        System.out.println("Best Fitness: " + String.format("%.2f", trees[0].getFitness()));
     }
 
     public void printBestTree() {
         System.out.println("Best Tree: " + trees[0]);
     }
 
-    // GP evolution (Checkpoint 2)
     public void evolve() {
         GPTree[] next = new GPTree[trees.length];
-
         for (int i = 0; i < trees.length; i += 2) {
             GPTree p1 = trees[rand.nextInt(trees.length / 2)].clone();
             GPTree p2 = trees[rand.nextInt(trees.length / 2)].clone();
@@ -55,9 +46,8 @@ public class Generation {
             p1.crossover(p2, rand);
 
             next[i] = p1;
-            if (i + 1 < trees.length) next[i + 1] = p2;
+            if(i + 1 < trees.length) next[i + 1] = p2;
         }
-
         trees = next;
     }
 }
