0c10d02b9f4b72656254124d49d89cc2884c234a
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-04 12:09:49 -0500

    TestGeneration.java

--- TestGeneration.java
+++ TestGeneration.java
@@ -1,28 +1,30 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TestGeneration {
-
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("You must provide a data file.");
-            return;
+        String fileName;
+
+        if (args.length > 0) fileName = args[0];
+        else {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter data file name: ");
+            fileName = sc.nextLine();
+            if (fileName.isEmpty()) {
+                System.out.println("You must provide a data file.");
+                return;
+            }
         }
 
-        String fileName = args[0];
         Generation gen = new Generation(500, 6, fileName);
-
-        // Evaluate all trees
         gen.evalAll();
 
-        // Print best tree
-        System.out.println("Best Tree: " + gen.getBestTree());
-
-        // Print best fitness
-        System.out.printf("Best Fitness: %.2f%n", gen.getBestFitness());
-
-        // Print top ten fitness values
         ArrayList<GPTree> top10 = gen.getTopTen();
-        System.out.print("Top Ten Fitness Values:\n");
+
+        System.out.println("Best Tree: " + top10.get(0));
+        System.out.printf("Best Fitness: %.2f%n", top10.get(0).getFitness());
+        System.out.println("Top Ten Fitness Values:");
+
         for (int i = 0; i < top10.size(); i++) {
             System.out.printf("%.2f", top10.get(i).getFitness());
             if (i != top10.size() - 1) System.out.print(", ");
