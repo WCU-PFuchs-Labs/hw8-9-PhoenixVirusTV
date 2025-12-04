380210218393f5430cfdd33ffcd821ae99cbe5c1
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-04 12:04:17 -0500

    TestGeneration.java

--- TestGeneration.java
+++ TestGeneration.java
@@ -1,23 +1,31 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class TestGeneration {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter data file name: ");
-        String file = in.nextLine();
 
-        Generation gen = new Generation(500, 6, file);
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("You must provide a data file.");
+            return;
+        }
+
+        String fileName = args[0];
+        Generation gen = new Generation(500, 6, fileName);
+
+        // Evaluate all trees
         gen.evalAll();
 
-        gen.printBestTree();
-        gen.printBestFitness();
+        // Print best tree
+        System.out.println("Best Tree: " + gen.getBestTree());
 
+        // Print best fitness
+        System.out.printf("Best Fitness: %.2f%n", gen.getBestFitness());
+
+        // Print top ten fitness values
         ArrayList<GPTree> top10 = gen.getTopTen();
         System.out.print("Top Ten Fitness Values:\n");
-
         for (int i = 0; i < top10.size(); i++) {
             System.out.printf("%.2f", top10.get(i).getFitness());
-            if (i < top10.size() - 1) System.out.print(", ");
+            if (i != top10.size() - 1) System.out.print(", ");
         }
         System.out.println();
     }
