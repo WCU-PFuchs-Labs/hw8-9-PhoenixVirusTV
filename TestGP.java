3557f21fc5c2e8f65bfc4af8bad9f39ae212869e
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-04 11:59:04 -0500

    Update

--- TestGP.java
+++ TestGP.java
@@ -1,6 +1,22 @@
 public class TestGP {
+
     public static void main(String[] args) {
-        // DO NOT PROMPT --- autograder breaks if you do
-        System.out.println("TestGP runs successfully.");
+        if (args.length < 1) {
+            System.out.println("You must provide a data file.");
+            return;
+        }
+
+        String fileName = args[0];
+        Generation g = new Generation(500, 6, fileName);
+
+        for (int gen = 1; gen <= 50; gen++) {
+            System.out.println("Generation " + gen + ":");
+
+            g.evalAll();
+            g.printBestTree();
+            g.printBestFitness();
+            g.printTopTen();
+            System.out.println(); // REQUIRED blank line
+        }
     }
 }
