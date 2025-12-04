e2f8e827708c6a56fff33d82cb9217860d4ecadd
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-03 20:32:25 -0500

    TestGP.java

--- TestGP.java
+++ TestGP.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 public class TestGP {
     public static void main(String[] args) {
+
+        // DO NOT PROMPT — autograder breaks if you do
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter data file name: ");
         String fileName = in.nextLine();
 
         Generation gen = new Generation(500, 6, fileName);
 
         for (int genNum = 1; genNum <= 50; genNum++) {
-            System.out.println("\nGeneration " + genNum + ":");
 
             gen.evalAll();
+
             gen.printBestTree();
             gen.printBestFitness();
+            gen.printTopTen();   // required by autograder
 
             gen.evolve();
         }
