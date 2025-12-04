648ac1dd55121fbe0d9ec957c285640f4173a690
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-04 12:15:09 -0500

    Generation.java

--- Generation.java
+++ Generation.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class Generation {
     private GPTree[] trees;
@@ -9,46 +7,53 @@ public class Generation {
     private Random rand;
 
     public Generation(int size, int maxDepth, String fileName) {
-        rand = new Random();
         data = new DataSet(fileName);
-
         Binop[] ops = { new Plus(), new Minus(), new Mult(), new Divide() };
-        factory = new NodeFactory(ops, data.getNumIndepVars());
-
+        factory = new NodeFactory(ops, data.getNumIndep());
+        rand = new Random();
         trees = new GPTree[size];
         for (int i = 0; i < size; i++) {
-            trees[i] = new GPTree(factory, maxDepth, rand);
+            trees[i] = new GPTree(factory, maxDepth, rand); // must match GPTree constructor
         }
     }
 
+    // Evaluate fitness for all trees and sort them
     public void evalAll() {
-        for (GPTree t : trees) t.evalFitness(data);
-        Arrays.sort(trees);
+        for (GPTree t : trees) {
+            t.evalFitness(data);
+        }
+        Arrays.sort(trees); // GPTree must implement Comparable<GPTree>
     }
 
-    public ArrayList<GPTree> getTopTen() {
-        ArrayList<GPTree> topTen = new ArrayList<>();
-        int n = Math.min(10, trees.length);
-        for (int i = 0; i < n; i++) topTen.add(trees[i].copy());
-        return topTen;
-    }
-
-    public void printBestFitness() {
-        if (trees.length > 0) System.out.printf("%.2f%n", trees[0].getFitness());
-    }
-
-    public void printBestTree() {
-        if (trees.length > 0) System.out.println(trees[0]);
-    }
-
-    // --- Methods the tests expect ---
+    // Return best tree (lowest fitness)
     public GPTree getBestTree() {
-        if (trees.length > 0) return trees[0].copy();
-        return null;
+        return trees[0];
     }
 
+    // Return best fitness
     public double getBestFitness() {
-        if (trees.length > 0) return trees[0].getFitness();
-        return Double.POSITIVE_INFINITY;
+        return trees[0].getFitness();
+    }
+
+    // Return top 10 trees as ArrayList
+    public ArrayList<GPTree> getTopTen() {
+        ArrayList<GPTree> top10 = new ArrayList<>();
+        for (int i = 0; i < Math.min(10, trees.length); i++) {
+            top10.add(trees[i]);
+        }
+        return top10;
+    }
+
+    // Evolve the generation (for TestGP)
+    public void evolve() {
+        GPTree[] newGen = new GPTree[trees.length];
+        for (int i = 0; i < trees.length; i += 2) {
+            GPTree parent1 = trees[rand.nextInt(trees.length)].clone();
+            GPTree parent2 = trees[rand.nextInt(trees.length)].clone();
+            parent1.crossover(parent2);
+            newGen[i] = parent1;
+            if (i + 1 < trees.length) newGen[i + 1] = parent2;
+        }
+        trees = newGen;
     }
 }
