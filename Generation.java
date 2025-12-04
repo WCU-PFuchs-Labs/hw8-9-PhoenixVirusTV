de3e64c7fc3c6ce77a2b54f32c6c250389a32a40
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-04 15:45:08 -0500

    Update Generation.java

--- Generation.java
+++ Generation.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Assume GPTree is your tree representation of expressions
 public class Generation {
     private GPTree[] trees;
     private DataSet data;
@@ -8,29 +9,33 @@ public class Generation {
 
     public Generation(int size, int maxDepth, String fileName) {
         data = new DataSet(fileName);
-
-        Binop[] ops = { new Plus(), new Minus(), new Mult(), new Divide() };
-        factory = new NodeFactory(ops, data.getNumIndepVars());
-
-        trees = new GPTree[size];
         rand = new Random();
 
+        // Set up operators
+        Binop[] ops = { new Plus(), new Minus(), new Mult(), new Divide() };
+        factory = new NodeFactory(ops, data.getNumIndepVars()); // <-- make sure DataSet has getNumIndepVars()
+
+        trees = new GPTree[size];
+
+        // Initialize trees randomly and evaluate fitness
         for (int i = 0; i < size; i++) {
-            trees[i] = new GPTree(factory, maxDepth, rand);
-            // Fitness is calculated internally; no need to call evaluate
+            trees[i] = new GPTree(factory, maxDepth, rand); // GPTree constructor needs Random
+            trees[i].evaluate(data); // <-- compute fitness here
         }
     }
 
+    // Return the tree with the lowest fitness
     public GPTree getBestTree() {
         GPTree best = trees[0];
         for (GPTree tree : trees) {
-            if (tree.getFitness() < best.getFitness()) {
+            if (tree.getFitness() < best.getFitness()) { // lower fitness = better
                 best = tree;
             }
         }
         return best;
     }
 
+    // Return top 10 fitness values sorted ascending
     public double[] getTopTenFitness() {
         double[] fitnessValues = new double[trees.length];
         for (int i = 0; i < trees.length; i++) {
