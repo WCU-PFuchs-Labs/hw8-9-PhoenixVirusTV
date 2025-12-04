6266f8c8cc69082c24d1a4701a21c42055fb6149
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-03 20:15:42 -0500

    GPTree.java

--- GPTree.java
+++ GPTree.java
@@ -1,100 +1,120 @@
-import java.util.ArrayList;
 import java.util.Random;
-private double fitness;
+import java.util.ArrayList;
 
-public class GPTree implements Collector {
+public class GPTree implements Comparable<GPTree> {
+
     private Node root;
-    private ArrayList<Node> crossNodes;
+    private NodeFactory nf;
+    private double fitness;
 
-    public GPTree() { root = null; }
+    // -------- CONSTRUCTORS --------
 
-    public GPTree(NodeFactory n, int maxDepth, Random rand) {
-        root = n.getOperator(rand);
-        root.addRandomKids(n, maxDepth, rand);
+    public GPTree(NodeFactory nf, int depth) {
+        this.nf = nf;
+        this.root = nf.randomTree(depth);
     }
 
-    /** Step 3: collect non-leaf nodes for crossover */
+    public GPTree(Node root, NodeFactory nf) {
+        this.root = root;
+        this.nf = nf;
+    }
+
+    // -------- EVALUATION OF A SINGLE ROW --------
+
+    public double eval(double[] data) {
+        return root.eval(data);
+    }
+
+    // -------- COLLECT FUNCTIONALITY --------
+
+    public void collect(Collector c) {
+        root.collect(c);
+    }
+
+    // -------- RANDOM SUBTREE SELECTION --------
+
+    private Node randomNode() {
+        ArrayList<Node> list = new ArrayList<>();
+        collect(n -> list.add(n));
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
+
+    // -------- CROSSOVER --------
+
+    public void crossover(GPTree other) {
+
+        // get random nodes from each tree
+        Node a = this.randomNode();
+        Node b = other.randomNode();
+
+        // clone b before swapping
+        Node bClone = (Node) b.clone();
+
+        // swap subtrees
+        if (a.getParent() == null) {
+            // a is root
+            this.root = bClone;
+            bClone.setParent(null);
+        } else {
+            a.getParent().replaceChild(a, bClone);
+        }
+    }
+
+
+    // -------- FITNESS EVALUATION --------
+
+    public void evalFitness(DataSet data) {
+        double sumSq = 0;
+
+        for (int i = 0; i < data.size(); i++) {
+            DataRow r = data.getRow(i);
+            double y = r.getY();
+            double[] x = r.getXValues();
+
+            double pred = eval(x);
+            double diff = pred - y;
+            sumSq += diff * diff;
+        }
+
+        fitness = sumSq;   // lower is better (MSE)
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
+
+    // -------- COMPARISON --------
+
     @Override
-    public void collect(Node node) {
-        if(!node.isLeaf()) {
-            if(crossNodes == null) crossNodes = new ArrayList<>();
-            crossNodes.add(node);
+    public int compareTo(GPTree t) {
+        return Double.compare(this.fitness, t.fitness);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof GPTree)) return false;
+        return this.compareTo((GPTree) o) == 0;
+    }
+
+    // -------- CLONE --------
+
+    @Override
+    public GPTree clone() {
+        try {
+            GPTree c = new GPTree(this.nf, 1);  // dummy depth: will be overwritten
+            c.root = (Node) this.root.clone();
+            c.fitness = this.fitness;
+            return c;
+        } catch (Exception e) {
+            return null;
         }
     }
 
-    /** prepare list of cross nodes by traversing tree */
-    public void traverse() {
-        crossNodes = new ArrayList<>();
-        root.traverse(this);
-    }
+    // -------- PRINTING --------
 
-    public String getCrossNodes() {
-        StringBuilder string = new StringBuilder();
-        int lastIndex = crossNodes.size() - 1;
-        for(int i = 0; i < lastIndex; ++i) {
-            Node node = crossNodes.get(i);
-            string.append(node.toString()).append(";");
-        }
-        if(lastIndex >= 0) string.append(crossNodes.get(lastIndex));
-        return string.toString();
-    }
-
-    public void crossover(GPTree tree, Random rand) {
-        this.traverse();
-        tree.traverse();
-        int thisPoint = rand.nextInt(this.crossNodes.size());
-        int treePoint = rand.nextInt(tree.crossNodes.size());
-        boolean left = rand.nextBoolean();
-
-        Node thisTrunk = crossNodes.get(thisPoint);
-        Node treeTrunk = tree.crossNodes.get(treePoint);
-
-        if(left) thisTrunk.swapLeft(treeTrunk);
-        else thisTrunk.swapRight(treeTrunk);
-    }
-
-    public String toString() { return root.toString(); }
-    public double eval(double[] data) { return root.eval(data); }
-}
-// Compute fitness over dataset
-public void evalFitness(DataSet data) {
-    double sumSq = 0;
-
-    for (int i = 0; i < data.size(); i++) {
-        DataRow r = data.getRow(i);
-        double y = r.getY();
-        double[] x = r.getXValues();
-
-        double pred = eval(x);
-        double diff = pred - y;
-        sumSq += diff * diff;
-    }
-
-    fitness = sumSq;
-}
-
-public double getFitness() {
-    return fitness;
-}
-
-@Override
-public int compareTo(GPTree t) {
-    return Double.compare(this.fitness, t.fitness);
-}
-
-@Override
-public boolean equals(Object o) {
-    if (o == null || !(o instanceof GPTree)) return false;
-    return compareTo((GPTree)o) == 0;
-}
-
-@Override
-public GPTree clone() {
-    try {
-        GPTree c = (GPTree) super.clone();
-        c.root = (Node) this.root.clone();
-        return c;
-    } catch (Exception e) {
-        return null;
+    public String toString() {
+        return root.toString();
     }
 }
